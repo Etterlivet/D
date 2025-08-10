4c99e82930059c9b53e360054f520cdfe321d8d9
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2025-08-10 19:18:05 +0200

    Update CenteredHero.tsx

--- src/features/landing/CenteredHero.tsx
+++ src/features/landing/CenteredHero.tsx
@@ -7,15 +7,15 @@ export const CenteredHero = (props: {
   <>
     <div className="text-center">{props.banner}</div>
 
-    <div className="mt-3 text-center text-5xl font-bold tracking-tight">
+    <div className="mt-3 text-center text-5xl font-heading tracking-tight">
       {props.title}
     </div>
 
-    <div className="mx-auto mt-5 max-w-screen-md text-center text-xl text-muted-foreground">
+    <div className="mx-auto mt-5 font-[EAU] max-w-screen-md text-center text-xl text-muted-foreground">
       {props.description}
     </div>
 
-    <div className="mt-8 flex justify-center gap-x-5 gap-y-3 max-sm:flex-col">
+    <div className="mt-8 flex font-[EAU] justify-center gap-x-5 gap-y-3 max-sm:flex-col">
       {props.buttons}
     </div>
   </>
