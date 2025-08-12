71f31bbfaebcd7a7fc36337c037f97d85dddc1e1
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2025-08-12 09:28:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,18 +7,25 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Roboto, Roboto_Mono  } from 'next/font/google'
 import '../styles/global.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+/ Define Roboto Sans
+const robotoSans = Roboto({
+  weight: ['400', '700'], // normal + bold
   subsets: ['latin'],
+  variable: '--font-roboto-sans',
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+// Define Roboto Mono
+const robotoMono = Roboto_Mono({
+  weight: ['400', '700'],
   subsets: ['latin'],
+  variable: '--font-roboto-mono',
 })
+
+
+
 
 export const metadata: Metadata = {
   title: 'Clerk Next.js Quickstart',
@@ -33,12 +40,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
               <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
               </SignUpButton>
